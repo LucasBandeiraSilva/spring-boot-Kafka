87a0b15c89f517dfb98c3c758dd13bb5072060b3
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-09-21 08:56:34 -0300

    [Pedidos] Lógica para persistir e solicitar pagamento do pedido

--- pedidos/src/main/java/com/lucasbandeira/icompras/pedidos/model/service/PedidoService.java
+++ pedidos/src/main/java/com/lucasbandeira/icompras/pedidos/model/service/PedidoService.java
@@ -1,11 +1,13 @@
 package com.lucasbandeira.icompras.pedidos.model.service;
 
+import com.lucasbandeira.icompras.pedidos.client.ServicoBancarioClient;
 import com.lucasbandeira.icompras.pedidos.model.Pedido;
 import com.lucasbandeira.icompras.pedidos.model.repository.ItemPedidoRepository;
 import com.lucasbandeira.icompras.pedidos.model.repository.PedidoRepository;
 import com.lucasbandeira.icompras.pedidos.validator.PedidoValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,10 +16,23 @@ public class PedidoService {
     private final PedidoRepository repository;
     private final ItemPedidoRepository itemPedidoRepository;
     private final PedidoValidator validator;
+    private final ServicoBancarioClient servicoBancarioClient;
 
+    @Transactional
     public Pedido criarPedido(Pedido pedido){
+        validator.validar(pedido);
+        realizarPersistencia(pedido);
+        enviarSolicitacaoPagamento(pedido);
+        return pedido;
+    }
+
+    private void enviarSolicitacaoPagamento( Pedido pedido ) {
+        String chavePagamento = servicoBancarioClient.solicitarPagamento(pedido);
+        pedido.setChavePagamento(chavePagamento);
+    }
+
+    private void realizarPersistencia( Pedido pedido ) {
         repository.save(pedido);
         itemPedidoRepository.saveAll(pedido.getItens());
-        return pedido;
     }
 }
