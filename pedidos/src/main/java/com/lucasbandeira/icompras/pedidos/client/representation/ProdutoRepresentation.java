4ea2a5decf71ab8bfa849d986c53f9e5636eae88
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-09-27 10:25:05 -0300

    Criando o client de Produtos

---
+++ pedidos/src/main/java/com/lucasbandeira/icompras/pedidos/client/representation/ProdutoRepresentation.java
@@ -0,0 +1,10 @@
+package com.lucasbandeira.icompras.pedidos.client.representation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+
+public record ProdutoRepresentation(Long codigo, String nome, BigDecimal valorUnitario) {
+}
--- pedidos/src/main/java/com/lucasbandeira/icompras/pedidos/validator/PedidoValidator.java
+++ pedidos/src/main/java/com/lucasbandeira/icompras/pedidos/validator/PedidoValidator.java
@@ -1,10 +1,20 @@
 package com.lucasbandeira.icompras.pedidos.validator;
 
+import com.lucasbandeira.icompras.pedidos.client.ProdutosClient;
+import com.lucasbandeira.icompras.pedidos.client.representation.ProdutoRepresentation;
 import com.lucasbandeira.icompras.pedidos.model.Pedido;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
+@RequiredArgsConstructor
 public class PedidoValidator {
 
-    public void validar( Pedido pedido){}
+    private final ProdutosClient produtosClient;
+
+    public void validar( Pedido pedido){
+    }
 }
