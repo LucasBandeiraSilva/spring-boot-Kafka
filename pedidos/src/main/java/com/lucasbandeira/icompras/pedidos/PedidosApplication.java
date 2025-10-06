756f036a6742ba8a40074614eb29e795456eab1f
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-10-06 09:17:09 -0300

    Conectando no Kafka e publicando mensagem de teste

--- pedidos/src/main/java/com/lucasbandeira/icompras/pedidos/PedidosApplication.java
+++ pedidos/src/main/java/com/lucasbandeira/icompras/pedidos/PedidosApplication.java
@@ -1,10 +1,19 @@
 package com.lucasbandeira.icompras.pedidos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class PedidosApplication {
+
+//	@Bean
+//	public CommandLineRunner commandLineRunner( KafkaTemplate<String,String>kafkaTemplate ){
+//		return args-> kafkaTemplate.send("icompras.pedidos-pagos","dados","Teste");
+//	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
